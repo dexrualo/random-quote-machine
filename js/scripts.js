894e4f2bd8f57e4fada4a8900766ab62fa08aefc
Dexter Rualo <dexterrualo@gmail.com>
2017-01-09 21:38:29 +0800

    Changed quote source

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
-  $("#getQuote").on("click", function(){
-
-    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(json) {
-      $("#quote").html(JSON.stringify(json));
+  $("#getQuote").on("click", function(e){
+    e.preventDefault();
+    $.ajax({
+      url: "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en",
+      success: function(json) {
+        console.log(json.quoteAuthor.length)
+        var quoteAuthor = json.quoteAuthor.length > 0 ? json.quoteAuthor : "Anonymous";
+        $("#quote").html(json.quoteText);
+        $("#title").html("-" + quoteAuthor);
+      },
+      cache: false
     });
   });
 });
