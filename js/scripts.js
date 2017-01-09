1e6a33447cf8771110ea57df5c9b573d4a966f70
Dexter Rualo <dexterrualo@gmail.com>
2017-01-09 22:20:46 +0800

    Added link to post to twitter

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,11 @@ $(document).ready(function(){
       url: "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en",
       success: function(json) {
         console.log(json.quoteAuthor.length)
+        var quoteText = json.quoteText;
         var quoteAuthor = json.quoteAuthor.length > 0 ? json.quoteAuthor : "Anonymous";
-        $("#quote").html(json.quoteText);
+        $("#quote").html(quoteText);
         $("#title").html("-" + quoteAuthor);
+        $('#tweet').attr('href', 'https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + quoteText + '" -' + quoteAuthor));
       },
       cache: false
     });
