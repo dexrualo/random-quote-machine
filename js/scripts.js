479040e2a2ae1316d1a5ac1e92a150d59e8913b9
Dexter Rualo <dexterrualo@gmail.com>
2017-01-09 20:42:58 +0800

    Fixed $.getJSON() call

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $("#getQuote").on("click", function(){
 
-    $.getJSON("http://api.forismatic.com/api/jsonp", function(json) {
+    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(json) {
       $("#quote").html(JSON.stringify(json));
     });
   });
