ea91b570a5230d6c5415028e7eaa6c75b80c1c9a
Dexter Rualo <dexterrualo@gmail.com>
2017-09-17 13:35:39 +0800

    Removed JSON.parse on responseE

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,14 @@ $(document).ready(function(){
   function getQuote() {
     $.ajax({
       headers: {
-        "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
+        "X-Mashape-Key": "7V1TK2FmVPmshuciTwwFBonG2wUGp1gHYn9jsnDcK005Exjds6",
         Accept: "application/json",
         "Content-Type": "application/x-www-form-urlencoded"
       },
-      url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
+      url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
       success: function(response) {
-        var r = JSON.parse(response);
-        quote = r.quote;
-        author = r.author;
+        quote = response.quote;
+        author = response.author;
         $(".quote").html(quote);
         $(".author").html("- " + author);
       }
